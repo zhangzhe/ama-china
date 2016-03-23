76d0fa04bef0d5fc5a9324a2a571620cd5ede9ff
zhangzhe <gary.zzhang@gmail.com>
2016-03-23 16:33:14 +0800

    improve UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :mentors
   devise_for :admins
-  root 'welcome#index'
+  root 'home#index'
   resources :ama_mentors, only: [:new, :create, :index, :show]
   # resources :amas_topics
 end
