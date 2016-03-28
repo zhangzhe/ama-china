335f584dc79dffb9119c6738da4be9f3c30d5025
zhangzhe <gary.zzhang@gmail.com>
2016-03-28 21:06:52 +0800

    home UI for phone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+  before_action :detect_browser, :only => [:index]
   def index
     @ama_mentors = AmaMentor.all
+  end
+
+  private
+  def detect_browser
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    else
+      request.variant = :desktop
+    end
   end
 end
